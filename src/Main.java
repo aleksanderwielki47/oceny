cbf64673bc4e2e52e762f1b08ea10c8f2d8a2863
aleksanderwielki47 <alekssioma410@gmail.com>
2024-11-21 13:47:15 +0100

    drugi

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,16 @@ abstract class Marks {
 }
 
 class A extends Marks {
-    int points1 = 35;
-    int points2 = 100;
-    int points3 = 78;
+    private int points1;
+    private int points2;
+    private int points3;
+
+    public A (int points1, int points2, int points3, int points4) {
+        this.points1 = points1;
+        this.points2 = points2;
+        this.points3 = points3;
+
+    }
     @Override
     void getPercentage() {
         int percentA = (points1 + points2 + points3) / 3;
@@ -13,10 +20,18 @@ class A extends Marks {
     }
 }
 class B extends Marks {
-    int points1 = 35;
-    int points2 = 100;
-    int points3 = 78;
-    int points4 = 37;
+    private int points1;
+    private int points2;
+    private int points3;
+    private int points4;
+
+    public B (int points1, int points2, int points3, int points4) {
+        this.points1 = points1;
+        this.points2 = points2;
+        this.points3 = points3;
+        this.points4 = points4;
+
+    }
 
     @Override
     void getPercentage() {
@@ -30,8 +45,8 @@ class B extends Marks {
 
 public class Main {
     public static void main(String[] args) {
-            A a = new A();
-            B b = new B();
+            A a = new A(15,49,57,100);
+            B b = new B(75,39,99,20);
             a.getPercentage();
             b.getPercentage();
         }
